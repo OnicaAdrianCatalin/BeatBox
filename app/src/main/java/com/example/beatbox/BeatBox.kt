8a5fb416f559313eef2e171b355d338f2625076a
Adrian Onica <>
2022-05-31 09:43:41 +0300

    fix handle exception in loadSounds

--- app/src/main/java/com/example/beatbox/BeatBox.kt
+++ app/src/main/java/com/example/beatbox/BeatBox.kt
@@ -1,6 +1,8 @@
 package com.example.beatbox
 
 import android.content.res.AssetManager
+import android.util.Log
+import java.io.IOException
 
 class BeatBox(private val assets: AssetManager) {
     val sounds: List<Sound>
@@ -10,15 +12,21 @@ class BeatBox(private val assets: AssetManager) {
     }
 
     private fun loadSounds(): List<Sound> {
-        return assets.list(SOUNDS_FOLDER)
-            ?.map { fileName ->
-                "$SOUNDS_FOLDER/$fileName"
-            }?.map { assetPath ->
-                Sound(assetPath)
-            } ?: emptyList()
+        return try {
+            assets.list(SOUNDS_FOLDER)
+                ?.map { fileName ->
+                    "$SOUNDS_FOLDER/$fileName"
+                }?.map { assetPath ->
+                    Sound(assetPath)
+                } ?: emptyList()
+        } catch (e: IOException) {
+            Log.e(TAG, "Could not list assets", e)
+            return emptyList()
+        }
     }
 
     companion object {
+        private const val TAG = "BeatBox"
         private const val SOUNDS_FOLDER = "sample_sounds"
     }
 }
